11ad4b6ca1e2de3fa89cdb69f72d1302e3c3647f
Lukz <lucaspgois@gmail.com>
2017-10-04 17:00:56 -0300

    Stylized maps CRUD

--- app/models/map.rb
+++ app/models/map.rb
@@ -1,5 +1,5 @@
 class Map < ApplicationRecord
-  has_many :matches, dependent: destroy
+  has_many :matches, dependent: :destroy
 
   validates :name, presence: true
   validates :exhibition_name, presence: true
